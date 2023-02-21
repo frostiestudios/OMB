aba90fa30d8989f3a16e9a664b98884a4b2c9866
thememetsar <mattwiner04@gmail.com>
2023-02-20 21:44:15 -0800

    Server

--- omb.py
+++ omb.py
@@ -5,6 +5,7 @@ from appJar import gui
 import webbrowser
 import threading
 import json
+import cgi
 
 def server(btn):
     if btn == "Start":
@@ -18,6 +19,24 @@ def server(btn):
         app.setLabel("SL", f"Server: http://{host_name}:{port_number}")
         app.setLabel("SS", "Server is Online")
         print("Server started at http://{}:{}/files".format(*httpd.socket.getsockname()))
+def do_upload(environ, start_response):
+    form = cgi.FieldStorage(
+        fp=environ['wsgi.input'],
+        environ=environ,
+        keep_blank_values=True
+    )
+
+    fileitem = form['fileToUpload']
+    if fileitem.filename:
+        # Make sure the file is saved with a unique name
+        filename = os.path.basename(fileitem.filename)
+        with open('/files/' + filename, 'wb') as f:
+            f.write(fileitem.file.read())
+        start_response('200 OK', [('Content-type', 'text/html')])
+        return [b'Thanks for uploading!']
+    else:
+        start_response('400 Bad Request', [('Content-type', 'text/html')])
+        return [b'No file was uploaded.']
 app=gui("OMB")
 app.addLabel("OMB RSLClient")
 app.startLabelFrame("Server General Commands")
