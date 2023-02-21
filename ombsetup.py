b005106bddb81c43edfd930eb75a1e962c7e326d
thememetsar <mattwiner04@gmail.com>
2023-02-20 20:57:30 -0800

    AMG R

---
+++ ombsetup.py
@@ -0,0 +1,15 @@
+import os
+from appJar import gui
+import webbrowser
+def save(btn):
+    print("Saved")
+app=gui("OMB Setup")
+app.addLabel("welcome")
+app.startLabelFrame("Location")
+app.addLabel("Choose a Place for the OMB media folder")
+app.addButton("Save", save)
+app.stopLabelFrame()
+app.startLabelFrame("2")
+app.addButton("Finish", save)
+app.stopLabelFrame()
+app.go()
