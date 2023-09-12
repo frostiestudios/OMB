b09be9e0abe2b101cbc8671864c6c96bdc3e9b5a
thememetsar <mattwiner04@gmail.com>
2023-09-11 18:39:18 -0700

    3

--- app.py
+++ app.py
@@ -4,15 +4,18 @@ import sqlite3
 import socket
 import os
 
-hostname = socket.gethostname()
-IPAddr = socket.gethostbyname(hostname)
+ip=socket.gethostbyname(socket.gethostname())
 
 bottle.BaseRequest.MEMFILE_MAX = 500000000
+@route('/pages/<filename:path>')
+def serve_static(filename):
+    return static_file(filename, root='./pages/')
+
 @route('/')
 def index():
-    return static_file('index.html',root='./pages/')
+    return template('./pages/index.html')
 
-@route('/videos')
+@route('/files')
 def videos():
     conn = sqlite3.connect('media.db')
     c = conn.cursor()
@@ -24,9 +27,21 @@ def videos():
 
 @route('/videos/upload', method='POST')
 def do_upload():
-    name, ext = os.path.splitext(upload.filename)
-    save_path = "./media/videos"
+    upload_name = request.forms.get('name')
+    upload = request.files.get('file')
+    if upload is not None:
+        name, ext = os.path.splitext(upload.filename)
+        save_path = "./media/videos"
     if not os.path.exists(save_path):
-        os.makedirs(saved_path)
-    upload.save(path)
-run(host=IPAddr,port=5500,debug=True,reloader=True)
+        os.makedirs(save_path)
+        upload.save(save_path)
+        conn = sqlite3.connect('media.db')
+        c = conn.cursor()
+        fname = name + ext
+        c.execute("INSERT INTO videos (name,file) VALUES (?,?)",(upload_name,upload))
+        conn.commit()
+        conn.close()
+        return redirect('/')
+    else:
+        return "ERROR NO FILE UPLOADED"
+run(host=ip,port=8888,debug=True,reloader=True)
